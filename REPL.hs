9bd0d423893f2720af57557c8b6d8f79b1663da5
vc36 <vc36@pc3-007-l.cs.st-andrews.ac.uk>
2018-02-12 15:39:57 +0000

    Update

--- REPL.hs
+++ REPL.hs
@@ -9,30 +9,45 @@ data State = State { vars :: [(Name, Int)],
                      history :: [Command] }
 
 initState :: State
-initState = State [] 0 []
+initState = State [("it",0)] 0 []
 
 -- Given a variable name and a value, return a new set of variables with
 -- that name and value added.
 -- If it already exists, remove the old value
 updateVars :: Name -> Int -> [(Name, Int)] -> [(Name, Int)]
-updateVars = undefined
+updateVars n x vars = dropVar n vars ++ [(n,x)]
 
 -- Return a new set of variables with the given name removed
 dropVar :: Name -> [(Name, Int)] -> [(Name, Int)]
-dropVar = undefined
+dropVar n vars = filter (\(a,b) -> a /= n) vars
 
 -- Add a command to the command history in the state
 addHistory :: State -> Command -> State
 addHistory st cmd = st {history = history st ++ [cmd]}
 
+getCmd :: [Command] -> Int -> Command
+getCmd cs n | length cs < n = error "Index too big"
+            | otherwise     = cs!!(n-1)
+
 process :: State -> Command -> IO ()
-process st (Set var e) 
-     = do let st' = undefined
-          -- st' should include the variable set to the result of evaluating e
-          repl st'
-process st (Eval e) 
+process st (Set var e)
+     = do let st' = addHistory st (Set var e)
+          if var /= "it" then do               -- protecting 'it' variable from modifications
+            putStrLn ("OK")
+            repl st' {vars = (updateVars var (fromJust (eval (vars st) e)) (vars st))}
+          else do
+            putStrLn("Cannot modify the implicit 'it' variable.")
+            repl st'
+process st (Eval e)
      = do let st' = addHistory st (Eval e)
-          repl st' {numCalcs = (fromMaybe 0 (eval (vars st') e))}
+          let it = fromJust (eval (vars st') e)
+          putStrLn (show it)
+          repl st' {numCalcs = numCalcs st + 1, vars = updateVars "it" it (vars st)}
+process st (AccessCmdHistory n)
+     = do let newCmd = getCmd (reverse (history st)) n
+          process st newCmd
+process st Quit
+     = putStrLn("Bye")
 
 -- Read, Eval, Print Loop
 -- This reads and parses the input using the pCommand parser, and calls
@@ -47,4 +62,3 @@ repl st = do putStr (show (numCalcs st) ++ " > ")
                           process st cmd
                   _ -> do putStrLn "Parse error"
                           repl st
-
